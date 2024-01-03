0bde2018706a65315e8b66135106a1f9d11438b0
Pedro Gisélio <60955300+pgiselio@users.noreply.github.com>
2024-01-03 07:34:43 -0300

    parece q nem tudo são flores e é pra ser exposto mesmo

--- services/suapapi/settings.js
+++ services/suapapi/settings.js
@@ -1,5 +1,5 @@
 export const SuapApiSettings = {
-    CLIENT_ID: process.env.SUAP_CLIENT_ID,
+    CLIENT_ID: "QnV7n3bSqFQS4GjO6M5ZOuRrRCf1O9DgD6rUBG2w",
     REDIRECT_URI: process.env.NODE_ENV === "development" ? "http://localhost:3000/auth/suap/" : "https://ifjobs-next.vercel.app/auth/suap/",
     SUAP_URL: "https://suap.ifrn.edu.br",
     SCOPE: "identificacao email documentos_pessoais",
