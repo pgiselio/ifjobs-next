d9b8bc58507838b2ab7196dda8cebbe1adfe093f
Pedro Gisélio <60955300+pgiselio@users.noreply.github.com>
2024-01-03 07:20:37 -0300

    como foi dito...

--- pages/sys/index.tsx
+++ pages/sys/index.tsx
@@ -9,7 +9,7 @@ import Head from "next/head";
 export default function HomePage() {
   const NewSuapClient = SuapClient({
     authHost: SuapApiSettings.SUAP_URL,
-    clientID: SuapApiSettings.CLIENT_ID,
+    clientID: SuapApiSettings.CLIENT_ID || "",
     redirectURI: SuapApiSettings.REDIRECT_URI,
     scope: SuapApiSettings.SCOPE,
   });
