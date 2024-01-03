c27df08790d5880412ee4f1df3e6e6beb1d62dd2
Pedro Gisélio <60955300+pgiselio@users.noreply.github.com>
2024-01-03 07:22:54 -0300

    como foi dito....

--- pages/auth/suap/index.tsx
+++ pages/auth/suap/index.tsx
@@ -5,7 +5,7 @@ import { SuapApiSettings } from "../../../services/suapapi/settings";
 export default function VerificarAlunoPage() {
   const NewSuapClient = SuapClient({
     authHost: SuapApiSettings.SUAP_URL,
-    clientID: SuapApiSettings.CLIENT_ID,
+    clientID: SuapApiSettings.CLIENT_ID || "",
     redirectURI: SuapApiSettings.REDIRECT_URI,
     scope: SuapApiSettings.SCOPE,
   });
