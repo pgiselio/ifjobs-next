c9f5d67e9fc40c3028ddd5946de1a610f2ec1ca5
Pedro Gisélio <60955300+pgiselio@users.noreply.github.com>
2024-01-21 08:10:47 -0300

    aa

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  output: "export",
   distDir: "dist",
   compiler: {
     styledComponents: true,
   },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig;
