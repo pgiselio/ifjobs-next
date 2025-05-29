3c4666bd5b18d727e348d74a062ea0c6f7c37759
Pedro Giselio <pedro.giselio.silva.707@ufrn.edu.br>
2025-05-29 00:45:27 -0300

    feat: adiciona utilização de envs

--- services/api.ts
+++ services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getUserLocalStorage } from "../contexts/AuthContext/util";
 
 export const api = axios.create({
-  baseURL: "https://ifjobs-backend.herokuapp.com",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
 api.interceptors.request.use(
--- services/suapapi/settings.js
+++ services/suapapi/settings.js
@@ -1,6 +1,6 @@
 export const SuapApiSettings = {
-    CLIENT_ID: "QnV7n3bSqFQS4GjO6M5ZOuRrRCf1O9DgD6rUBG2w",
-    REDIRECT_URI: process.env.NODE_ENV === "development" ? "http://localhost:3000/auth/suap/" : "https://ifjobs-next.vercel.app/auth/suap/",
-    SUAP_URL: "https://suap.ifrn.edu.br",
-    SCOPE: "identificacao email documentos_pessoais",
+    CLIENT_ID: process.env.NEXT_PUBLIC_SUAP_CLIENT_ID,
+    REDIRECT_URI: process.env.NEXT_PUBLIC_SUAP_REDIRECT,
+    SUAP_URL: process.env.NEXT_PUBLIC_SUAP_URL,
+    SCOPE: process.env.NEXT_PUBLIC_SUAP_SCOPE,
 }
